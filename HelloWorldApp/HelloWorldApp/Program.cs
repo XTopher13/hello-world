4426c4ffe1baff398af4fb9c618b6806171f0413
Christopher Hanson <XTopher.Hanson13@gmail.com>
2025-08-10 20:11:04 -0500

    5 | Use old template and implement GetTimeOfDay

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Convert to Unit Tests:
+            /*
+            DateTime now = DateTime.Now;
+            DateTime morningTest = new DateTime(now.Year, now.Month, now.Day, 8,00,00);
+            DateTime afternoonTest = new DateTime(now.Year, now.Month, now.Day, 14,00,00);
+            DateTime eveningTest = new DateTime(now.Year, now.Month, now.Day, 20,00,00);
+            */
+            Console.WriteLine($"Good {GetTimeOfDay()}, Christopher!");
+        }
+
+        public static string GetTimeOfDay()
+        {
+            return GetTimeOfDay(DateTime.Now);
+        }
+
+        public static string GetTimeOfDay(DateTime now)
+        {
+            Console.WriteLine($"{now}");
+            DateTime noon = new DateTime(now.Year, now.Month, now.Day, 12, 00, 00);
+            DateTime evening = new DateTime(now.Year, now.Month, now.Day, 17, 00, 00);
+
+            if (now < noon) return "Morning";
+            if (now < evening) return "Afternoon";
+            return "Evening";
+        }
+    }
+}
